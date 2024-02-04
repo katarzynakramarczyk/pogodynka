e8ddf0646c71565c0d8efaca7b85e2940dfe0195
Katarzyna Kramarczyk <katarzyna_kramarczyk@zut.edu.pl>
2024-02-04 10:54:28 +0100

    lab-c

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
@@ -7,8 +6,11 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,27 +2,34 @@
 
 namespace App\Controller;
 
-use App\Entity\Location;
-use App\Repository\MeasurementRepository;
-use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use App\Repository\ForecastRepository;
+use App\Repository\LocationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{country}/{city}', name: 'app_weather', requirements: ['id' => '\d+'])]
-    public function city(
-        #[MapEntity(mapping: ['country' => 'country', 'city' => 'city'])]
-        Location $location,
-        MeasurementRepository $repository,
-    ): Response
+    #[Route('/weather/{city}/{country?}', name: 'app_weather')]
+    public function city(string $city, ?string $country, LocationRepository $locationrepository, ForecastRepository $forecastRepository): Response
     {
-        $measurements = $repository->findByLocation($location);
+        $locationVal =['city'=> $city];
+        if($country){
+            $locationVal['country'] = $country;
+        }
+
+        $location = $locationrepository->findOneBy($locationVal);
+
+        if (!$location){
+            throw $this->createNotFoundException('Nie odnaleziono wskazanego miasta');
+        }
+
+        $forecasts = $forecastRepository->findByLocation($location);
 
         return $this->render('weather/city.html.twig', [
+            'controller_name' => 'WeatherController',
             'location' => $location,
-            'measurements' => $measurements,
+            'forecasts' => $forecasts,
         ]);
     }
 }
